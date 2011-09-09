d593e63b5eaefd037586cfa0e111f18d3c8f91ee
Marcin Chylek <marcin@chylek.pl>
2011-09-09 17:01:25 +0200

    move files

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 git submodule update --init --recursive
 
 cd docs
+mkdir -p source
 cp -R ../symfony-docs-pl/* source/
 make html
 
-cp -R source/_static/* build/html/
+cp -R _static/* build/html/
+rm -R source
